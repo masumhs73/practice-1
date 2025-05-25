e8bf1f31df01eecf26b5e4ac4177530ceaf53cd7
masumhs73 <masumshuvo58@gmail.com>
2025-05-25 11:25:01 +0600

    added nav and category button

---
+++ script/index.js
@@ -0,0 +1,12 @@
+function loadCategories() {
+    // console.log("Connected!")
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+        .then(res => res.json())
+        .then((data) => displayCategories(data.categories))
+}
+
+function displayCategories(categories){
+    console.log(categories);
+}
+
+loadCategories();
